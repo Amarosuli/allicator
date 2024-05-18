6c28f42e7b121ad31d37c0cea02e03354f7239f2
Amarosuli <vuvlnvw@gmail.com>
2024-05-18 20:00:04 +0700

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto flex w-full flex-col space-y-8">
+	<div class="flex h-20 items-center justify-between border-b px-4">
+		<div>Allicator</div>
+		<div class="flex space-x-4">
+			<a href="/login">Login</a>
+			<a href="/">Home</a>
+		</div>
+	</div>
+	<slot></slot>
+	<div class="flex w-full items-center justify-center space-x-4 pt-12"></div>
+</div>
+
+<style></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Allicator</h1>
