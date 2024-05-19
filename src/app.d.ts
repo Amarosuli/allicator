9139b13cb050fe83937dc7b49b85d7fb447fcaac
Amarosuli <vuvlnvw@gmail.com>
2024-05-19 23:32:37 +0700

    [add] hooks.server.ts - config the pocketbase instance

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import PocketBase, { type AuthModel } from 'pocketbase';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: PocketBase;
+			user: AuthModel | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
