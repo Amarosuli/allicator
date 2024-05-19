39e2453a822a0756db2dc02aaf8a9ee39dea5836
Amarosuli <vuvlnvw@gmail.com>
2024-05-19 20:52:48 +0700

    [update] login page content

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,31 @@
-<div>Login Page</div>
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+
+	let employeeId = '';
+	let password = '';
+</script>
+
+<div class="flex w-full flex-col items-center justify-center gap-2">
+	<div class="text-2xl font-extrabold">Login Page</div>
+
+	<div class="w-1/3 space-y-2 p-8">
+		<Label class="font-semibold" for="employeeId">Employee ID</Label>
+		<Input
+			bind:value={employeeId}
+			type="text"
+			id="employeeId"
+			name="employeeId"
+			placeholder="Insert Your ID"
+		/>
+		<Label class="font-semibold" for="password">Password</Label>
+		<Input
+			bind:value={password}
+			type="text"
+			id="password"
+			name="password"
+			autocomplete="false"
+			placeholder="Insert Your Password"
+		/>
+	</div>
+</div>
