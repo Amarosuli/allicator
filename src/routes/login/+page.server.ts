f0112ca0d92f586efa356b4448b65b32049f9ce1
Amarosuli <vuvlnvw@gmail.com>
2024-05-21 19:36:40 +0700

    [add] login page

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,27 @@
+import { loginSchema } from '$lib/zodSchema.js';
+import { fail, redirect } from '@sveltejs/kit';
+import { superValidate, message } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+import type { Actions, PageServerLoad } from '../$types.js';
+
+export const load: PageServerLoad = async () => {
+	return { form: await superValidate(zod(loginSchema)) };
+};
+
+export const actions: Actions = {
+	default: async ({ locals, request }) => {
+		const form = await superValidate(request, zod(loginSchema));
+
+		if (!form.valid) return fail(400, { form });
+
+		try {
+			await locals.pb.collection('users').authWithPassword(form.data.employeeId, form.data.password);
+		} catch (error: any) {
+			const errorMessage = `${error?.response.message} Crosscheck your ID and Password are typed correctly or make sure your ID already registered.`;
+			return message(form, errorMessage, { status: error?.status });
+		}
+
+		throw redirect(302, '/');
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,31 +1,42 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input';
-	import { Label } from '$lib/components/ui/label';
+	import { Button } from '$lib/components/ui/button';
+	import { FieldErrors, Control, Field, Label } from '$lib/components/ui/form';
+	import { superForm } from 'sveltekit-superforms';
 
-	let employeeId = '';
-	let password = '';
+	export let data;
+	const form = superForm(data.form);
+	const { form: formData, message, enhance } = form;
 </script>
 
-<div class="flex w-full flex-col items-center justify-center gap-2">
+<div class="flex w-full flex-col items-center justify-center">
 	<div class="text-2xl font-extrabold">Login Page</div>
-
-	<div class="w-1/3 space-y-2 p-8">
-		<Label class="font-semibold" for="employeeId">Employee ID</Label>
-		<Input
-			bind:value={employeeId}
-			type="text"
-			id="employeeId"
-			name="employeeId"
-			placeholder="Insert Your ID"
-		/>
-		<Label class="font-semibold" for="password">Password</Label>
-		<Input
-			bind:value={password}
-			type="text"
-			id="password"
-			name="password"
-			autocomplete="false"
-			placeholder="Insert Your Password"
-		/>
+	<div class="flex w-1/3 flex-col items-center justify-center space-y-3 p-8">
+		<form class="flex w-full max-w-80 flex-col" method="post" use:enhance>
+			<Field {form} name="employeeId">
+				<Control let:attrs>
+					<Label>Employee ID</Label>
+					<Input {...attrs} bind:value={$formData.employeeId} placeholder="Your Employee ID" />
+				</Control>
+				<FieldErrors />
+			</Field>
+			<Field {form} name="password">
+				<Control let:attrs>
+					<Label>Password</Label>
+					<Input {...attrs} bind:value={$formData.password} type="password" placeholder="Your Password" />
+				</Control>
+				<FieldErrors />
+			</Field>
+			<Button type="submit">Let's go!</Button>
+			{#if $message}
+				<p class="mt-2 bg-red-200 p-2 text-center text-xs font-semibold">{$message}</p>
+			{/if}
+		</form>
+		<div class="flex w-full flex-col items-center justify-center space-y-2 p-8">
+			<div class="flex w-full justify-between">
+				<p class="w-full text-ellipsis text-center text-sm">Share your thoughts!</p>
+			</div>
+			<Button class="w-fit text-xs" variant="outline">Feedback & Improvement</Button>
+		</div>
 	</div>
 </div>
