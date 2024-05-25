5b7fcbf11cc07433c86e45d193d27e2a71a61b58
Amarosuli <vuvlnvw@gmail.com>
2024-05-25 18:45:45 +0700

    [add] function to generate file url

--- src/lib/pocketbaseClient.ts
+++ src/lib/pocketbaseClient.ts
@@ -1,4 +1,12 @@
 import { PUBLIC_PB_URL } from '$env/static/public';
-import PocketBase from 'pocketbase';
+import PocketBase, { type RecordModel } from 'pocketbase';
 
-export const pb = new PocketBase(PUBLIC_PB_URL);
+export const pb: PocketBase = new PocketBase(PUBLIC_PB_URL);
+
+type Option = {
+	pocketbase?: PocketBase;
+	thumb?: string;
+};
+export const getFileUrl = (record: RecordModel, option: Option): string => {
+	return option.pocketbase ? option.pocketbase.files.getUrl(record, record.documents[0], { thumb: option.thumb }) : pb.files.getUrl(record, record.documents[0], { thumb: option.thumb });
+};
