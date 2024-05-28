8a2d036d19852ddbcbebc84fcb2a7fdf8ca20ab9
Amarosuli <vuvlnvw@gmail.com>
2024-05-28 15:54:49 +0700

    [update]  extend type from the PocketBase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import PocketBase, { type AuthModel } from 'pocketbase';
+import type { Customer, EngineFamily, EngineList, EngineModel, Unit } from '$lib/CostumTypes';
+import PocketBase, { type AuthModel, RecordService } from 'pocketbase';
+
+interface TypedPocketBase extends PocketBase {
+	collection(idOrName: string): RecordService;
+	collection(idOrName: 'engine_families'): RecordService<EngineFamily>;
+	collection(idOrName: 'engine_models'): RecordService<EngineModel>;
+	collection(idOrName: 'customers'): RecordService<Customer>;
+	collection(idOrName: 'units'): RecordService<Unit>;
+	collection(idOrName: 'engine_list'): RecordService<EngineList>;
+}
 
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			pb: PocketBase;
+			pb: TypedPocketBase;
 			user: AuthModel | undefined;
 		}
 		// interface PageData {}
