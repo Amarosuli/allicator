0cef65102d9d0d4561d5d214cc7bc1a1816638f0
Amarosuli <vuvlnvw@gmail.com>
2024-05-28 15:55:36 +0700

    [update] +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
-	<div class="absolute inset-0 flex flex-col items-center justify-center">
+	<div class="absolute inset-0 flex animate-pulse flex-col items-center justify-center bg-red-200">
 		<img class="w-80" src="/ooops.png" alt="ooops" />
-		<p class="pt-4 text-2xl font-extrabold uppercase text-red-600">{$page.status} - {$page.error?.message}</p>
-		<p class="pb-4 text-xl font-semibold text-slate-800">The page you are looking for doesn't exist</p>
+		<p class="text-md pt-4 font-extrabold uppercase text-red-500">{$page.status} - {$page.error?.message}</p>
+		<p class="pb-4 text-sm font-semibold text-slate-800">The page you are looking for doesn't exist</p>
 		<Button variant="default" href="/">Back to Home</Button>
 	</div>
 </div>
