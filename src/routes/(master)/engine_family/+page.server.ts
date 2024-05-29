2b6d368c71d2d471f011573d14f886fd82c36d5c
Amarosuli <vuvlnvw@gmail.com>
2024-05-30 00:24:38 +0700

    [add] engine_family page

---
+++ src/routes/(master)/engine_family/+page.server.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const getEngineFamily = async () => {
+		return await locals.pb.collection('engine_families').getList(1, 6);
+	};
+	return {
+		engineFamily: await getEngineFamily()
+	};
+};
---
+++ src/routes/(master)/engine_family/+page.svelte
@@ -0,0 +1,237 @@
+<script lang="ts">
+	import DataTableCheckbox from '$lib/components/costum/data-table-checkbox.svelte';
+	import DataTableActions from '$lib/components/costum/data-table-actions.svelte';
+
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Table from '$lib/components/ui/table';
+	import { addHiddenColumns, addSelectedRows, addSortBy, addTableFilter } from 'svelte-headless-table/plugins';
+	import { ArrowDown, ArrowUp, ChevronDown, LoaderCircle, PlusCircle } from 'lucide-svelte';
+	import { Render, Subscribe, createRender, createTable } from 'svelte-headless-table';
+	import { writable } from 'svelte/store';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { fade } from 'svelte/transition';
+	import { pb } from '$lib/pocketbaseClient.js';
+	import { cn } from '$lib/utils.js';
+
+	import type { RecordModel } from 'pocketbase';
+
+	export let data;
+	const { engineFamily, user } = data;
+
+	let isDataLoading = writable<boolean>(false);
+	let hasNextPage: boolean;
+	let hasPrevPage: boolean;
+	let currentPage = writable(0);
+	let totalPages = writable(1);
+	let items = writable<RecordModel[]>([]);
+
+	$: $items = engineFamily.items;
+	$: $currentPage = engineFamily.page;
+	$: $totalPages = engineFamily.totalPages;
+
+	$: hasPrevPage = $currentPage != 1 && $currentPage <= $totalPages ? true : false;
+	$: hasNextPage = $currentPage < $totalPages ? true : false;
+
+	async function nextPage() {
+		$isDataLoading = true;
+		let result = await pb.collection('engine_families').getList($currentPage + 1, 6);
+		$totalPages = result.totalPages;
+		$items = result.items;
+		$currentPage = result.page;
+		$isDataLoading = false;
+	}
+
+	async function prevPage() {
+		$isDataLoading = true;
+		let result = await pb.collection('engine_families').getList($currentPage - 1, 6);
+		$totalPages = result.totalPages;
+		$items = result.items;
+		$currentPage = result.page;
+		$isDataLoading = false;
+	}
+
+	const table = createTable(items, {
+		sort: addSortBy({ disableMultiSort: true }),
+		filter: addTableFilter({
+			fn: ({ filterValue, value }) => value.includes(filterValue)
+		}),
+		select: addSelectedRows(),
+		hide: addHiddenColumns()
+	});
+
+	const columns = table.createColumns([
+		table.column({
+			header: (_, { pluginStates }) => {
+				const { allPageRowsSelected } = pluginStates.select;
+				return createRender(DataTableCheckbox, {
+					checked: allPageRowsSelected
+				});
+			},
+			accessor: 'id',
+			cell: ({ row }, { pluginStates }) => {
+				const { getRowState } = pluginStates.select;
+				const { isSelected } = getRowState(row);
+
+				return createRender(DataTableCheckbox, {
+					checked: isSelected
+				});
+			},
+			plugins: {
+				sort: {
+					disable: true
+				},
+				filter: {
+					exclude: true
+				}
+			}
+		}),
+		table.column({
+			header: 'Name',
+			accessor: 'name',
+			plugins: { filter: { exclude: true } }
+		}),
+		table.column({
+			header: 'Description',
+			accessor: 'description',
+			plugins: {
+				filter: {
+					getFilterValue(value) {
+						return value.toLowerCase();
+					}
+				}
+			}
+		}),
+		table.column({
+			header: '',
+			accessor: ({ id }) => id,
+			cell: (item) => {
+				return createRender(DataTableActions, { id: item.value, user });
+			},
+			plugins: {
+				sort: {
+					disable: true
+				}
+			}
+		})
+	]);
+
+	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, flatColumns, pluginStates, rows } = table.createViewModel(columns);
+	const { selectedDataIds } = pluginStates.select;
+	const { hiddenColumnIds } = pluginStates.hide;
+	const { filterValue } = pluginStates.filter;
+	const { sortKeys } = pluginStates.sort;
+
+	const ids = flatColumns.map((c) => c.id);
+	let hideForId = Object.fromEntries(ids.map((id) => [id, true]));
+
+	$: $hiddenColumnIds = Object.entries(hideForId)
+		.filter(([, hide]) => !hide)
+		.map(([id]) => id);
+
+	const hideableCols = ['name', 'description'];
+</script>
+
+<svelte:head>
+	<title>Engine Families</title>
+</svelte:head>
+
+<div class="mx-auto h-max w-full border p-4">
+	<div class="flex w-full items-center justify-between">
+		<p class="w-full font-extrabold lg:text-xl">Engine Families</p>
+		{#if user}
+			<Button size="sm" href="/engine_family/create">
+				<div class="flex items-center gap-2">
+					Add <PlusCircle class="h-4 w-4" />
+				</div>
+			</Button>
+		{:else}
+			<p class="block w-full text-ellipsis text-right text-xs">Login to manage data</p>
+		{/if}
+	</div>
+	<div class="flex items-center gap-2 py-2">
+		<Input class="max-w-sm" placeholder="Filter name..." type="text" bind:value={$filterValue} />
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button variant="outline" class="ml-auto" builders={[builder]}>
+					Columns <ChevronDown class="ml-2 h-4 w-4" />
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				{#each flatColumns as col}
+					{#if hideableCols.includes(col.id)}
+						<DropdownMenu.CheckboxItem bind:checked={hideForId[col.id]}>
+							{col.header}
+						</DropdownMenu.CheckboxItem>
+					{/if}
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	</div>
+	<div class="relative rounded-md border">
+		{#if $isDataLoading}
+			<div transition:fade={{ duration: 200 }} class="absolute inset-0 z-30 flex flex-col items-center justify-center bg-secondary/80">
+				<LoaderCircle class="animate-spin text-yellow-400" />
+				<p class="pt-2 text-sm text-yellow-300">Loading Data</p>
+			</div>
+		{/if}
+		<Table.Root {...$tableAttrs}>
+			<Table.Header>
+				{#each $headerRows as headerRow}
+					<Subscribe rowAttrs={headerRow.attrs()}>
+						<Table.Row>
+							{#each headerRow.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
+									<Table.Head {...attrs} class={cn('[&:has([role=checkbox])]:pl-3')}>
+										{#if cell.id === 'name'}
+											<Button variant="ghost" on:click={props.sort.toggle}>
+												<Render of={cell.render()} />
+												{#if $sortKeys[0]?.id === cell.id && $sortKeys[0]?.order === 'asc'}
+													<ArrowDown class="ml-2 h-4 w-4 text-foreground" />
+												{:else if $sortKeys[0]?.id === cell.id && $sortKeys[0]?.order === 'desc'}
+													<ArrowUp class="ml-2 h-4 w-4 text-foreground" />
+												{:else}
+													<div class="ml-2 h-4 w-4"></div>
+												{/if}
+											</Button>
+										{:else}
+											<Render of={cell.render()} />
+										{/if}
+									</Table.Head>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Header>
+			<Table.Body {...$tableBodyAttrs}>
+				{#each $pageRows as row (row.id)}
+					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+						<Table.Row {...rowAttrs} data-state={$selectedDataIds[row.id] && 'selected'}>
+							{#each row.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs>
+									<Table.Cell class="[&:has([role=checkbox])]:pl-3" {...attrs}>
+										{#if cell.id === 'name'}
+											<div class="ml-4 capitalize">
+												<Render of={cell.render()} />
+											</div>
+										{:else}
+											<Render of={cell.render()} />
+										{/if}
+									</Table.Cell>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+	<div class="flex items-center justify-end space-x-2 py-4">
+		<div class="flex-1 text-sm text-muted-foreground">
+			Page {$currentPage} of {$totalPages}
+		</div>
+		<Button variant="outline" size="sm" on:click={prevPage} disabled={!hasPrevPage}>Previous</Button>
+		<Button variant="outline" size="sm" disabled={!hasNextPage} on:click={nextPage}>Next</Button>
+	</div>
+</div>
