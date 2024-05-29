df1db51ad86683071a50336ca07227b222b5eece
Amarosuli <vuvlnvw@gmail.com>
2024-05-30 00:22:16 +0700

    [update] add engineFamilySchema

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -4,3 +4,8 @@ export const loginSchema = z.object({
 	employeeId: z.string().trim().min(6, 'Employee Id is required, Minimal 6 Characters'),
 	password: z.string().trim().min(8, 'Minimal password is 8 Characters')
 });
+
+export const engineFamilySchema = z.object({
+	name: z.string().trim().min(2, 'Family name is required, Minimal 2 Characters'),
+	description: z.string().trim().optional()
+});
