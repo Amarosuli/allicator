bdb452b73c8e1e52b0f1027c2af2f70583000095
Amarosuli <vuvlnvw@gmail.com>
2024-06-10 00:36:17 +0700

    [add] data-table-avatar.svelte

---
+++ src/lib/components/costum/data-table-avatar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar';
+	import { getInitial } from '$lib/utils';
+
+	export let alt: string;
+	export let src: string;
+	export let fallback: string;
+</script>
+
+<Avatar.Root class="h-9 w-9">
+	<Avatar.Image {src} {alt} />
+	<Avatar.Fallback class="uppercase">{getInitial(fallback)}</Avatar.Fallback>
+</Avatar.Root>
