cebefda4b79f45d7d86cab152c39c6a27f587e87
Amarosuli <vuvlnvw@gmail.com>
2024-06-11 23:05:24 +0700

    [remove] index.test.ts

--- src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { getFirstPath } from '$lib/helpers';
-import { describe, it, expect } from 'vitest';
-
-describe('Get first pathname', () => {
-	const pathname = '/engine_family/create?id="abcdefg"';
-	it('get the first pathname', () => {
-		expect(getFirstPath(pathname)).equal('/engine_family');
-	});
-});
