162633dde5b504ab0f8ea2f63db8e196a43f6ab4
Amarosuli <vuvlnvw@gmail.com>
2024-06-11 12:56:55 +0700

    [add] data-table-image component

---
+++ src/lib/components/costum/data-table-image.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { AspectRatio } from '$lib/components/ui/aspect-ratio';
+	export let alt: string;
+	export let src: string;
+</script>
+
+<div class="w-9">
+	<AspectRatio ratio={1 / 1} class="flex items-center justify-center">
+		<img {src} {alt} class="w-full object-cover" />
+	</AspectRatio>
+</div>
