8dcdc711c24f93f0296dd690f6ace438e0a50249
Amarosuli <vuvlnvw@gmail.com>
2024-07-03 01:39:02 +0700

    [update] Add date input functionality

--- src/routes/(master)/project_list/edit/+page.svelte
+++ src/routes/(master)/project_list/edit/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+	import * as Popover from '$lib/components/ui/popover';
 	import * as Select from '$lib/components/ui/select';
 
+	import { DateFormatter, parseDate, getLocalTimeZone, type DateValue } from '@internationalized/date';
 	import { FieldErrors, Control, Field, Label } from '$lib/components/ui/form';
-	import { LoaderCircle } from 'lucide-svelte';
+	import { LoaderCircle, CalendarIcon } from 'lucide-svelte';
 	import { getFirstPath } from '$lib/helpers.js';
 	import { superForm } from 'sveltekit-superforms';
+	import { Calendar } from '$lib/components/ui/calendar';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { toast } from 'svelte-sonner';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import { cn } from '$lib/utils.js';
 
 	export let data;
 	const { project, engineModels, engineList, customers, projectTypes } = data;
@@ -24,10 +28,20 @@
 	const { form: formData, delayed, message, enhance } = form;
 	const basePath = getFirstPath($page.url.pathname);
 
+	const df = new DateFormatter('en-US', {
+		dateStyle: 'long'
+	});
+
 	const projectStatus = [
 		{ label: 'OPEN', value: 'OPEN' },
 		{ label: 'CLOSED', value: 'CLOSED' }
 	];
+
+	let startedAt: DateValue | undefined;
+	let finishedAt: DateValue | undefined;
+
+	$: startedAt = $formData.started_at && $formData.started_at !== 'undefined' ? parseDate($formData.started_at) : undefined;
+	$: finishedAt = $formData.finished_at && $formData.finished_at !== 'undefined' ? parseDate($formData.finished_at) : undefined;
 
 	$: project &&
 		formData.set({
@@ -36,10 +50,8 @@
 			engine_id: project.expand!.engine_id.id,
 			customer: project.expand!.customer.id,
 			project_type_id: project.expand!.project_type_id.id,
-			revision_number: project.revision_number,
-			status: project.description,
-			started_at: project.started_at,
-			finished_at: project.finished_at
+			started_at: new Date(project.started_at).toISOString().split('T')[0],
+			finished_at: new Date(project.finished_at).toISOString().split('T')[0]
 		});
 
 	$: selectedModel = $formData.engine_model_id
@@ -79,7 +91,7 @@
 </svelte:head>
 
 <div class="flex w-full flex-col items-center justify-center">
-	<div class="text-2xl font-extrabold">Project</div>
+	<div class="text-2xl font-extrabold">Edit Project</div>
 	<div class="flex w-full flex-col items-center justify-center space-y-3 p-8 md:w-1/3">
 		<form class="flex w-full max-w-80 flex-col" method="post" use:enhance>
 			<Field {form} name="revision_number">
@@ -172,14 +184,14 @@
 
 			<Field {form} name="project_type_id">
 				<Control let:attrs>
-					<Label>Engine Model</Label>
+					<Label>Project Type</Label>
 					<Select.Root
 						selected={selectedProjectType}
 						onSelectedChange={(v) => {
 							v && ($formData.project_type_id = v.value);
 						}}>
 						<Select.Trigger>
-							<Select.Value class="w-full text-start" placeholder="Select Engine Model" />
+							<Select.Value class="w-full text-start" placeholder="Select Project Type" />
 						</Select.Trigger>
 						<Select.Content class="max-h-60 overflow-y-auto">
 							<Select.Group>
@@ -222,6 +234,54 @@
 						</Select.Content>
 						<Select.Input {...attrs} bind:value={$formData.status} />
 					</Select.Root>
+				</Control>
+				<FieldErrors class="text-xs italic" />
+			</Field>
+
+			<Field {form} name="started_at">
+				<Control let:attrs>
+					<Label>Started at</Label>
+					<Popover.Root>
+						<Popover.Trigger asChild let:builder>
+							<Button variant="outline" class={cn('w-[280px] justify-start text-left font-normal', !startedAt && 'text-muted-foreground')} builders={[builder]}>
+								<CalendarIcon class="mr-2 h-4 w-4" />
+								{startedAt ? df.format(startedAt.toDate(getLocalTimeZone())) : 'Pick a date'}
+							</Button>
+						</Popover.Trigger>
+						<Popover.Content class="w-auto p-0">
+							<Calendar
+								bind:value={startedAt}
+								onValueChange={(v) => {
+									$formData.started_at = v ? v.toString() : '';
+								}}
+								initialFocus />
+						</Popover.Content>
+					</Popover.Root>
+					<input type="text" hidden value={$formData.started_at} {...attrs} />
+				</Control>
+				<FieldErrors class="text-xs italic" />
+			</Field>
+
+			<Field {form} name="finished_at">
+				<Control let:attrs>
+					<Label>Finished at</Label>
+					<Popover.Root>
+						<Popover.Trigger asChild let:builder>
+							<Button variant="outline" class={cn('w-[280px] justify-start text-left font-normal', !finishedAt && 'text-muted-foreground')} builders={[builder]}>
+								<CalendarIcon class="mr-2 h-4 w-4" />
+								{finishedAt ? df.format(finishedAt.toDate(getLocalTimeZone())) : 'Pick a date'}
+							</Button>
+						</Popover.Trigger>
+						<Popover.Content class="w-auto p-0">
+							<Calendar
+								bind:value={finishedAt}
+								onValueChange={(v) => {
+									$formData.finished_at = v ? v.toString() : '';
+								}}
+								initialFocus />
+						</Popover.Content>
+					</Popover.Root>
+					<input type="text" hidden value={$formData.finished_at} {...attrs} />
 				</Control>
 				<FieldErrors class="text-xs italic" />
 			</Field>
