593dd6fd9c74abf7ab592b16c1754feb3bfb7194
vuvlnvw <vuvlnvw@gmail.com>
2024-07-24 06:48:25 +0000

    [update] get value from .env

--- src/lib/pocketbaseClient.ts
+++ src/lib/pocketbaseClient.ts
@@ -1,10 +1,10 @@
-import { PUBLIC_PB_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import PocketBase from 'pocketbase';
 
 import type { FileUrlOption, TypedPocketBase } from './CostumTypes';
 
-export const pb = new PocketBase(PUBLIC_PB_URL).autoCancellation(false) as TypedPocketBase;
+export const pb = new PocketBase(env.PUBLIC_PB_URL).autoCancellation(false) as TypedPocketBase;
 
 export const getFileUrl = (collectionIdOrCollectionName: string, id: string, filename: string, option?: FileUrlOption): string => {
-	return `${PUBLIC_PB_URL}/api/files/${collectionIdOrCollectionName}/${id}/${filename}?thumb=${option?.thumb}&download=${option?.download ? 1 : 0}`;
+	return `${env.PUBLIC_PB_URL}/api/files/${collectionIdOrCollectionName}/${id}/${filename}?thumb=${option?.thumb}&download=${option?.download ? 1 : 0}`;
 };
